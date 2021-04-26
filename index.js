162d957c42ee2effba7a2db4ccaafb502874756e
figo5530 <figooooo0221@gmail.com>
2021-04-26 23:25:21 +0800

    commit tree dd2a07dcf71dcb547cfe6896bab44ea3c59db7d3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function saturdayFun(act="roller-skate") {
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(act="go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(sign="*") {
+    return function (params="special") {
+        return `You are ${sign}${params}${sign}!`
+    }
+}
+const Calculator = {add: (a, b) => a + b, subtract: (a, b) => a - b, multiply: (a, b) => a * b, divide: (a, b) => a / b}
+
+function actionApplyer(startingInt, arrFnc) {
+    let a = startingInt
+
+    for (let i = 0; i < arrFnc.length; i++) {
+        a = arrFnc[i](a)
+    }
+    return a
+}
